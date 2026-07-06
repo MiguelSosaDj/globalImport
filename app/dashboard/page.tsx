f2092d076c2762a348523f63152f398d83f4ca7f
sosaprue25 <sosaprue25@gmail.com>
2026-07-06 15:40:41 -0500

    ajustes ejemplo

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -27,6 +27,8 @@ export default async function DashboardPage() {
     .eq("user_id", user.id)
     .single();
 
+
+    
   const { data: citas } = await supabase
     .from("citas")
     .select("*")
